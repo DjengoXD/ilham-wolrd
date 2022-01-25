d1a61d688533c1150a246c2279b2173c236f5187
DjengoXD <93562913+DjengoXD@users.noreply.github.com>
2022-01-25 11:01:46 +0700

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>  <img alt="Vue " src="../assets/o.png"></h1>
+  </div>
+</template>
+<script>
+import riwayat
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>  <div class="home">
+    <img alt="Vue " src="../assets/o.jpg">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    this.getData()
+  
+  },
+  methods: {
+    async getData(){
+      let{data, error} = await this.$supabase
+      .from("tb_produk")
+      .select();
+      if(data) console.log(data)
+      if(error) console.error(error)
+    }
+  }
+}
+</script>
