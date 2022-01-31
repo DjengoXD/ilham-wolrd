35fc2e70978285fd207730754cf3c50fc254d416
DjengoXD <93562913+DjengoXD@users.noreply.github.com>
2022-01-31 13:17:10 +0700

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div  class="\">
+
+ 
+    <h1>  <img alt="Vue " src="../assets/download.jpeg"></h1>
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
